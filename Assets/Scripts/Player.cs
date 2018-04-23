8026a836d9d4c6c87617dd4e92692b448c5da3dc
Helden,Laura L.H.A. van <l.vanhelden@student.fontys.nl>
2018-04-23 13:04:51 +0200

    starting

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    Controller2D controller;
+    private void Start()
+    {
+        controller = GetComponent<Controller2D>();
+    }
+}
